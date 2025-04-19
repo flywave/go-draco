8bf58a9d8509f5b8afb51fc28a9cc1e741356ca1
hj <in_blu@163.com>
2025-04-19 08:58:42 +0800

    update

--- decoder.go
+++ decoder.go
@@ -3,13 +3,13 @@ package draco
 // #include <stdlib.h>
 // #include <string.h>
 // #include "draco_api.h"
-// #cgo CFLAGS: -I ./lib
-// #cgo CXXFLAGS: -I ./lib
-// #cgo linux,amd64 LDFLAGS: -L ./lib/linux -Wl,--start-group  -lstdc++ -lm -pthread -ldraco -lc_draco -Wl,--end-group
-// #cgo linux,arm64 LDFLAGS: -L ./lib/linux_arm -Wl,--start-group  -lstdc++ -lm -pthread -ldraco -lc_draco -Wl,--end-group
-// #cgo windows LDFLAGS: -L ./lib/windows  -Wl,--start-group  -lstdc++  -ldraco -lc_draco -Wl,--end-group
-// #cgo darwin,amd64 LDFLAGS: -L　./lib/darwin -ldraco -lc_draco -lc++
-// #cgo darwin,arm64 LDFLAGS: -L　./lib/darwin_arm -ldraco -lc_draco -lc++
+// #cgo CFLAGS: -I ./libs
+// #cgo CXXFLAGS: -I ./libs
+// #cgo linux,amd64 LDFLAGS: -L ./libs/linux -Wl,--start-group  -lstdc++ -lm -pthread -ldraco -lc_draco -Wl,--end-group
+// #cgo linux,arm64 LDFLAGS: -L ./libs/linux_arm -Wl,--start-group  -lstdc++ -lm -pthread -ldraco -lc_draco -Wl,--end-group
+// #cgo windows LDFLAGS: -L ./libs/windows  -Wl,--start-group  -lstdc++  -ldraco -lc_draco -Wl,--end-group
+// #cgo darwin,amd64 LDFLAGS: -L　./libs/darwin -ldraco -lc_draco -lc++
+// #cgo darwin,arm64 LDFLAGS: -L　./libs/darwin_arm -ldraco -lc_draco -lc++
 import "C"
 import (
 	"runtime"
